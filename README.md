36f791c09a46df3d97fc9cf2e0bf1893516770ae
CeliaFernandez <celia.fernandez.madrazo@cern.ch>
2020-07-24 09:02:54 +0200

    Add instructions and files to run crab3 in slc7 machine

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # globalDisplaced-Analyzer
+
+
+
+## Crab3 launching in slc7
+
+Some modifications must be done to launch crab3 jobs when using a slc7 machine.
+
+First, assure that you are working with a CMSSW release and arch compatible with crab3 by checking in here: https://cmssdt.cern.ch/SDT/cgi-bin/ReleasesXML
+
+Declare that jobs will be launched with crab standalone [slc7]:
+
+```
+source /cvmfs/cms.cern.ch/crab3/crab_slc7_standalone.sh
+```
+
+When working with crab in slc7 the attribute 'instance' in the 'ConfigSection' object will be missed and you will need to declare it by adding this line to the crab config file:
+``` 
+config.General.instance = 'prod'
+```
+
---
+++ test/DYJetsToLL_M-50_crabSubmission.py
@@ -0,0 +1,27 @@
+from WMCore.Configuration import Configuration
+config = Configuration()
+
+config.section_('General')
+config.General.transferLogs = True
+config.General.requestName = 'DYJetsToLL_M-50_DGNTuple'
+config.General.instance = 'prod'
+
+config.section_('JobType')
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'runDGAnalysis_cfg.py'
+config.JobType.disableAutomaticOutputCollection = True
+config.JobType.outputFiles = ['output.root']
+config.JobType.maxMemoryMB = 2500
+
+config.section_('Data')
+config.Data.inputDBS = 'phys03'
+#config.Data.unitsPerJob = 1
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 10
+#config.Data.totalUnits = 50
+config.Data.inputDataset = '/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/fernance-DYJetsToLL_M-50_HXX_RunIISummer16MiniAODv3_modified-bd3e7bcff6c9bcad356ea4ed7e4f08b4/USER'
+config.Data.publication = False
+config.Data.outLFNDirBase = '/store/user/fernance/' 
+
+config.section_('Site')
+config.Site.storageSite = 'T2_ES_IFCA'
